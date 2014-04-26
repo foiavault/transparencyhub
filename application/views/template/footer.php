45b096e76dcbc17716821b21a6c3150d97a68b6e
bl4ckdu5t <joerexko@gmail.com>
2014-04-26 08:43:32 -0500

    Added bootstrap and jquery

---
+++ application/views/template/footer.php
@@ -0,0 +1,4 @@
+<script src="/transparencyhub/assets/js/jquery.js"></script>
+<script src="/transparencyhub/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/transparencyhub/assets/css/bootstrap.min.css">
+
+</head>
+<body>
